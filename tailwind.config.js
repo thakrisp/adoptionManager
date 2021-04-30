95bdd077e65fcab62222fd1ac98928c41dc3a853
Ryan C <Ryancrispin@gmail.com>
2021-04-30 18:26:18 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
